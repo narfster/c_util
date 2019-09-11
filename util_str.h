8ed1894d6aff248cf98ff54b0cfb7cc1445f7b07
Dan Yerushalmi <dan@irpsystems.com>
2019-09-11 17:26:17 +0300

    adding first files and functions to this repo

---
+++ util_str.h
@@ -0,0 +1,48 @@
+//! \file util_str.h
+//!
+//! \author Dan Yerushalmi https://github.com/narfster
+//!
+//! \date 11/09/2019
+//!
+//! \brief string utilites
+//!
+//! \copyright MIT License 2019 Dan Yerushalmi (c)
+//! 
+//! Permission is hereby granted, free of charge, to any person obtaining a copy
+//! of this software and associated documentation files (the "Software"), to deal
+//! in the Software without restriction, including without limitation the rights
+//! to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//! copies of the Software, and to permit persons to whom the Software is
+//! furnished to do so, subject to the following conditions:
+//!
+//! The above copyright notice and this permission notice shall be included in all
+//! copies or substantial portions of the Software.
+//! 
+//! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//! IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//! FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//! AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//! LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//! OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//! SOFTWARE.
+
+
+#pragma once 
+
+//! Split string by delimiter
+//!
+//! \arg str -  String to split must be mutable
+//! \arg argv[] - point to tokens stored in str 
+//! \arg argv_len - length of given argv array
+//! \arg split_count - number of tokens found
+//! \arg delim - the characters to use as delimiter
+//! 
+//! fucntion will add null characters between tokens into orginal string
+//! if you don't want function to touch original string, send in a copy.
+//! argv An array of char pointers, each char pointer will point to
+//! first character of token in char* str
+void util_str_split(char* str ,
+                    char* argv[],
+                    int argv_len, 
+                    int* split_count, 
+                    const char* delim);
